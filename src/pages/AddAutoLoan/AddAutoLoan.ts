447219b7a88d21e4226cdd7a674a3c18f9a123b6
sanjeevpleta <34168820+sanjeevpleta@users.noreply.github.com>
2017-12-02 19:56:55 -0500

    Add files via upload

---
+++ src/pages/AddAutoLoan/AddAutoLoan.ts
@@ -0,0 +1,14 @@
+﻿import { Component } from '@angular/core';
+
+import { NavController } from 'ionic-angular';
+
+@Component({
+    selector: 'page-auto',
+    templateUrl: 'addautoloan.html'
+})
+export class AddAutoLoan {
+
+    constructor(public navCtrl: NavController) {
+
+    }
+}
---
+++ src/pages/AddPersonal/AddPersonalLoan.ts
@@ -0,0 +1,14 @@
+﻿import { Component } from '@angular/core';
+
+import { NavController } from 'ionic-angular';
+
+@Component({
+    selector: 'page-personal',
+    templateUrl: 'AddPersonalLoan.html'
+})
+export class AddPersonalLoan {
+
+    constructor(public navCtrl: NavController) {
+
+    }
+}
