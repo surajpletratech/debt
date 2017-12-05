55512a100f31e3bf0330d0909ed85891a6796fa2
sanjeevpleta <34168820+sanjeevpleta@users.noreply.github.com>
2017-12-04 22:01:34 -0500

    Add files via upload

--- src/pages/AddHomeLoan/addhomeloan.ts
+++ src/pages/AddHomeLoan/addhomeloan.ts
@@ -32,50 +32,50 @@ export class AddHomeLoan {
   }
   
   Calculate() {
-	this.myData = this.slideOneForm.value;
-	if (this.myData.startDate.length === 0) {
-		this.myData.startDate = new Date();
-	}
-	//this.monthlyPayment = 100000 * ( ( ((4.5 * 0.01)/12) *  Math.pow( 1 + ((4.5 * 0.01)/12), 360 ))  / ( Math.pow(1 + ((4.5 * 0.01)/12), 360 )  ))
-	let mp = ((((this.myData.rate * 0.01) / 12) / (1 - (Math.pow((1 + ((this.myData.rate * 0.01) / 12)), -(this.myData.term * 12))))) * this.myData.loanAmount);
-	this.monthlyPayment = Math.round(mp)
-	this.totalPayment = mp * this.myData.term * 12;
-	this.totalInterest = this.totalPayment - this.myData.loanAmount;
+	  this.myData = this.slideOneForm.value;
+	  if (this.myData.startDate.length === 0) {
+		  this.myData.startDate = new Date();
+	  }
+	  //this.monthlyPayment = 100000 * ( ( ((4.5 * 0.01)/12) *  Math.pow( 1 + ((4.5 * 0.01)/12), 360 ))  / ( Math.pow(1 + ((4.5 * 0.01)/12), 360 )  ))
+	  let mp = ((((this.myData.rate * 0.01) / 12) / (1 - (Math.pow((1 + ((this.myData.rate * 0.01) / 12)), -(this.myData.term * 12))))) * this.myData.loanAmount);
+	  this.monthlyPayment = Math.round(mp)
+	  this.totalPayment = mp * this.myData.term * 12;
+	  this.totalInterest = this.totalPayment - this.myData.loanAmount;
   }
   
   prepareObject() : IHomeLoans {
-	let self = this;
-	self.Calculate();
+	  let self = this;
+	  self.Calculate();
 	
-	let homeloan: IHomeLoans = {
-		key: 1 ,
-		Name:'NA' ,
-		Address:'NA' ,
-		Amount:this.myData.loanAmount ,
-		Rate:this.myData.rate ,
-		Term:this.myData.term ,
-		StartDate:this.myData.startDate,
-		LenderName: 'NA',
-		ScheduledPayment:this.monthlyPayment,
-		ScheduledNumberofPayment:this.myData.term * 12,
-		TotalInterest:this.totalInterest,
-		TotalAmountPaid:this.totalPayment,
-		TotalInterestPaid:this.totalInterest,
-		APR: 0
-	};
+	  let homeloan: IHomeLoans = {
+		  key: 1 ,
+		  Name:'NA' ,
+		  Address:'NA' ,
+		  Amount:this.myData.loanAmount ,
+		  Rate:this.myData.rate ,
+		  Term:this.myData.term ,
+		  StartDate:this.myData.startDate,
+		  LenderName: 'NA',
+		  ScheduledPayment:this.monthlyPayment,
+		  ScheduledNumberofPayment:this.myData.term * 12,
+		  TotalInterest:this.totalInterest,
+		  TotalAmountPaid:this.totalPayment,
+		  TotalInterestPaid:this.totalInterest,
+		  APR: 0
+	  };
 	
-	return homeloan;
+	  return homeloan;
   }
   
   CalculateAmortization() {
-	let homeloan = this.prepareObject();
+	  let homeloan = this.prepareObject();
 	
-	//this.navCtrl.push(Amortization, {homeLoankey: 1});
-	this.navCtrl.push(Amortization, {homeLoanData: homeloan});
+	  //this.navCtrl.push(Amortization, {homeLoankey: 1});
+	  this.navCtrl.push(Amortization, {homeLoanData: homeloan});
   }
   
   saveHomeLoan() {
-	let homeloan = this.prepareObject();
+	  let homeloan = this.prepareObject();
   }
 
 }
